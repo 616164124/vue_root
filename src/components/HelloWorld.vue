4336e0aad37b1c8a59fb69a4ce5fc21b15547ec6
hua <616164124@qq.com>
2021-07-22 04:38:12 +0800

    2021年7月22日04:38:05

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <il v-for=" itme in alist">{{itme}}</il>
+    <il v-for=" itme in alist">{{itme}}</il>
+    <il v-if="judgment">jfjl</il>
+    <il v-else>1111</il>
+    <il v-if="!judgment">ppppp</il>
+    <il v-else>1111</il>
   </div>
 </template>
 
@@ -88,7 +15,9 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      alist:['a','b','v','d'],
+      judgment: false
     }
   }
 }
---
+++ src/components/main/Main.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="main" >
+      <il>main</il>
+      <div v-bind:class="">main----</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Main.vue",
+        components: {},
+        props: {},
+        data() {
+            return {
+              isActive: false
+            };
+        },
+        created() {
+        },
+        mounted() {
+        },
+        activited() {
+        },
+        update() {
+        },
+        beforeRouteUpdate() {
+        },
+        methods: {},
+        filter: {},
+        computed: {},
+        watch: {},
+    };
+
+</script>
+
+<style scoped>
+
+</style>
